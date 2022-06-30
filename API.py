d61dc6971a73b00d7a5d3ca88b83573b8108b6c3
thrivenienugu <enuguthriveni@gmail.com>
2022-06-30 15:12:20 +0200

    Adding API.py file

---
+++ API.py
@@ -0,0 +1,8 @@
+import requests
+def get_phone(name):
+    r = requests.get(f'http://localhost:5000/api?action=phone&name={name}')
+    print(r.text)
+
+def get_name(phone):
+    r = requests.get(f'http://localhost:5000/api?action=name&phone={phone}')
+    print(r.text)
